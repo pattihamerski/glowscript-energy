84e44e7339b321303cf3bbbe342e402cacdcc7cf
paulhamerski <68252665+paulhamerski@users.noreply.github.com>
2020-10-20 09:48:04 -0400

    Update energy-transfer.js

--- energy-transfer.js
+++ energy-transfer.js
@@ -2,4 +2,12 @@ function foo() {
     return 42;
 }
 
-var running = true;
+function system_pairs(systems) {
+    system_pairs = [];
+    for (i = 0; i < systems.length; i++) { 
+        for (j = i+1; j < systems.length; j++) {
+            system_pairs.push([systems[i], systems[j]]);
+        }
+    }
+    return system_pairs;
+}
