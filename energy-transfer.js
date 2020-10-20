e6cd324e7b5fea4bb997b6d4b4256b906b6031b8
paulhamerski <68252665+paulhamerski@users.noreply.github.com>
2020-10-20 09:35:15 -0400

    Update energy-transfer.js

--- energy-transfer.js
+++ energy-transfer.js
@@ -1,3 +1,17 @@
 function foo() {
     return 42;
 }
+
+running = True;
+pause_button = button(text="Pause", bind=pause_it, pos=scene.title_anchor);
+function pause_it() {
+    nonlocal running;
+    if running {
+        pause_button.text = "Resume";
+        running = False;
+    }
+    else {
+        pause_button.text = "Pause";
+        running = True;
+    }
+}
